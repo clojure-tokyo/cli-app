9fa7f4059a5f4d0280225b48406486effa0433b5
rbf <rbf@users.noreply.github.com>
2017-08-14 20:24:30 +0900

    new: build and run jar file

--- test/cli_app/core_test.clj
+++ test/cli_app/core_test.clj
@@ -1,7 +1,13 @@
 (ns cli-app.core-test
   (:require [clojure.test :refer :all]
-            [cli-app.core :refer :all]))
+            [cli-app.core :refer :all]
+            [clojure.java.shell]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest basic-integration
+  (testing "that the command runs"
+    (is
+      (= (clojure.java.shell/sh "java" "-jar" "target/uberjar/cli-app-0.1.0-SNAPSHOT-standalone.jar" "george")
+          { :exit 0,
+            :out "Hello george!\n"
+            :err "" }))))
+
