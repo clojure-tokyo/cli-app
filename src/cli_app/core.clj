4ebff30c55f2b3e96761d97179a38a30cd700668
rbf <rbf@users.noreply.github.com>
2017-08-14 22:46:53 +0900

    new: first implementation of expr parsing

--- src/cli_app/core.clj
+++ src/cli_app/core.clj
@@ -2,10 +2,49 @@
   (:gen-class)
   (:require [clojure.string :as s]))
 
-(defn- calculate
+(defn translate
+  [x]
+  (condp re-matches x
+    #"[+*-]" (keyword x)
+    #"\d+"   (Integer/parseInt x)
+    x))
+
+(defn- ensure-parsing
+  [coll]
+  (if-let [unprocessed (->> coll
+                            (drop-while (comp not string?))
+                            (s/join " ")
+                            not-empty)]
+    {:error       "Unexpected symbol"
+     :unprocessed unprocessed}
+    coll))
+
+(defn parse
+  [s]
+  (->> (s/split s #"\b")
+        (map s/trim)
+        (filter (comp not empty?))
+        (map translate)
+        ensure-parsing
+        not-empty))
+
+(defn- error
   [expr]
-  "10")
+  (when-let [e (:error expr)]
+    (str e " We stopped parsing here: " (:unprocessed expr))))
+
+(defn calculate
+  [expr]
+  (or
+    (error expr)
+    10))
 
 (defn -main
   [& args]
-  (println (calculate (s/join " " args))))
+  (->> args
+       (s/join " ")
+       parse
+       calculate
+       println))
+
+
--- test/cli_app/core_test.clj
+++ test/cli_app/core_test.clj
@@ -11,3 +11,24 @@
             :out "10\n"
             :err "" }))))
 
+(deftest transparency
+  (testing "that the same number is returned"
+    (are [s expected] (= (calculate s) expected)
+      "10"  10)))
+
+(deftest parsing
+  (testing "that the parsing works"
+    (are [s expected] (= (parse s) expected)
+      ""          nil
+      "2"         [2]
+      "1 10 0 +"  [1 10 0 :+]
+      "1 2+ 3 *"  [1 2 :+ 3 :*]
+      "0-1+2*3"   [0 :- 1 :+ 2 :* 3])))
+
+(deftest parsing-errors
+  (testing "that the parsing properly handles errors"
+    (are [s expected] (= (parse s) expected)
+      "/"         {:error       "Unexpected symbol"
+                   :unprocessed "/"}
+      "1 2 / 4 5" {:error       "Unexpected symbol"
+                   :unprocessed "/ 4 5"})))
